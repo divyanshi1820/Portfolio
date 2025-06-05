8a467ef332d394a169259519e5572825a7053e1c
divyanshi1820 <divyanshibhandari18@gmail.com>
2025-06-06 04:18:06 +0530

    Create carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+<script>
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.carousel-img');
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  // Show the first slide on load
+  showSlide(currentSlide);
+</script>

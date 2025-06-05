bbf1aa5fb0291053b052220d2eb23d79856c91aa
divyanshi1820 <divyanshibhandari18@gmail.com>
2025-06-06 04:27:36 +0530

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,24 @@
 <script>
-  let currentSlide = 0;
-  const slides = document.querySelectorAll('.carousel-img');
+ let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-img');
 
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      slide.classList.toggle('active', i === index);
-    });
-  }
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
 
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-  }
-
-  function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
-  }
-
-  // Show the first slide on load
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentSlide);
+});
 </script>
